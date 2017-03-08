dea83b616f0b2ead205f58e8358bd3472a32eb35
Cristian Barbaro <cbarbaro@cert.unlp.edu.ar>
2017-03-08 14:20:57 -0300

    se borra un anuncio bgp

--- ConfExaBGP/parseroute.py
+++ ConfExaBGP/parseroute.py
@@ -24,44 +24,27 @@ while True:
 	message = json.loads(line)
 
 	if message["type"] == "update":
-		
-		try:	
-			peer = message['neighbor']['address']['peer']
-			net  = message['neighbor']['message']['update']['announce']['ipv4 unicast'][peer]
-			ip = net[0]['nlri']
+		try:
+			update_hash = message['neighbor']['message']['update']
+			if 'announce' in update_hash.keys():
+				peer = message['neighbor']['address']['peer']
+				array  = message['neighbor']['message']['update']['announce']['ipv4 unicast'][peer]
+				action = "add"
+				message_type = "announce"
+			elif 'withdraw' in update_hash.keys():
+				array  = message['neighbor']['message']['update']['withdraw']['ipv4 unicast']
+				action = "del"
+				message_type = "withdraw"
+
+			net = array[0]['nlri']
 			asn = message['neighbor']['asn']['peer']
-	 				
-		 	# log some shit
-			f = open('/home/redes/Desktop/workfile', 'a+')			
-			f.write("\n LOG:" + str(message)+"\n")
-			f.close()
-		
+			value = "{0} route {1} next-hop self origin igp as-path [{2}]".format(message_type, net, asn)
+
 			# Announce received ExaBGP-route trough Quagga peering	
-			if (ip):
-				#value = "announce route "+ ip + " next-hop self"
+			if (net):
 				# Pagina para la sintaxis: https://thepacketgeek.com/advanced-router-peering-and-route-announcement/
-				value = "announce route {0} next-hop self origin igp as-path [{1}]".format(ip,asn)
-				post = requests.post('http://localhost:5000/', data = {'command':value})
-				out = os.system("ip route add "+str(ip)+" dev "+str(asn))
-
-
-		except KeyError: 
-			continue
-
-		try:	
-			peer = message['neighbor']['address']['peer']
-			net  = message['neighbor']['message']['update']['withdraw']['ipv4 unicast'][peer]
-			ip = net[0]['nlri']
-			asn = message['neighbor']['asn']['peer']
-
-			# Withdraw received ExaBGP-route trough Quagga peering	
-			if (ip):
-				#value = "announce route "+ ip + " next-hop self"
-				# Pagina para la sintaxis: https://thepacketgeek.com/advanced-router-peering-and-route-announcement/
-				value = "withdraw route {0} ".format(ip)
-				post = requests.post('http://localhost:5000/', data = {'command':value})
-				out = os.system("ip route del "+str(ip)+" dev "+str(asn))
-
+				post = requests.post('http://localhost:5000/', data = {'command' : value})
+				out = os.system("ip route {0} {1} dev {2}".format(action, net, asn))
 
 		except KeyError: 
 			continue
