4fa44ef24704288ceaf9a8acfc7f73707127f5a3
Cristian Barbaro <cbarbaro@cert.unlp.edu.ar>
2017-03-29 18:15:47 -0300

    correccion en parseroute para eliminar un anuncio

--- ConfExaBGP/parseroute.py
+++ ConfExaBGP/parseroute.py
@@ -31,15 +31,19 @@ while True:
 				array  = message['neighbor']['message']['update']['announce']['ipv4 unicast'][peer]
 				action = "add"
 				message_type = "announce"
+				asn = update_hash['attribute']['as-path'][0]
 			elif 'withdraw' in update_hash.keys():
 				array  = message['neighbor']['message']['update']['withdraw']['ipv4 unicast']
 				action = "del"
 				message_type = "withdraw"
+				asn=""
 
 			net = array[0]['nlri']
-			#asn = message['neighbor']['asn']['peer']
-			asn = update_hash['attribute']['as-path'][0]
-			value = "{0} route {1} next-hop self origin igp as-path [{2}]".format(message_type, net, asn)
+
+			if message_type == "announce":
+				value = "{0} route {1} next-hop self origin igp as-path [{2}]".format(message_type, net, asn)
+			elif message_type == "withdraw":
+				value = "{0} route {1}".format(message_type, net)
 
 			# Announce received ExaBGP-route trough Quagga peering
 			if (net):
@@ -47,5 +51,6 @@ while True:
 				post = requests.post('http://localhost:5000/', data = {'command' : value})
 				out = os.system("ip route {0} {1} dev {2}".format(action, net, asn))
 
-		except KeyError:
-			continue
+		except KeyError as detail:
+			pass
+
