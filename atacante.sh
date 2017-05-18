7a89e3e115538546d0e39b7ef3e9add0a7253669
Cristian Barbaro <cbarbaro@cert.unlp.edu.ar>
2017-05-18 11:17:53 -0300

    cambio de nombre de cooperativa a isp

--- atacante.sh
+++ atacante.sh
@@ -5,9 +5,9 @@ source .env
 core_path=$(ps aux | grep -oP "/tmp/pycore.[0-9]+" | head -n 1)
 
 # Pings contra dos servidores de cada organizacion:
-/usr/sbin/vcmd -c $core_path/n24 -- bash -E -c "ping 163.10.10.4" &>/dev/null & 
-/usr/sbin/vcmd -c $core_path/n24 -- bash -E -c "ping 193.81.7.10" &>/dev/null &
+/usr/sbin/vcmd -c $core_path/n24 -- bash -E -c "ping 182.23.0.10" &>/dev/null & 
+#/usr/sbin/vcmd -c $core_path/n24 -- bash -E -c "ping 193.81.7.10" &>/dev/null &
 
 # Hping con SYN TCP al puerto 80:
-/usr/sbin/vcmd -c $core_path/n24 -- bash -E -c "hping3 -S 163.10.10.4 -p 80" &>/dev/null &
-/usr/sbin/vcmd -c $core_path/n24 -- bash -E -c "hping3 -S 193.81.7.10 -p 80" &>/dev/null &
+/usr/sbin/vcmd -c $core_path/n24 -- bash -E -c "hping3 -S 182.23.0.10 -p 80" &>/dev/null &
+#/usr/sbin/vcmd -c $core_path/n24 -- bash -E -c "hping3 -S 193.81.7.10 -p 80" &>/dev/null &
